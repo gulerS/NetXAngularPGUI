ae802eb19f65d3e942230840f7c947e465b42a9b
Sercan <sercanguler@live.com>
2023-03-15 01:26:45 +0300

    first commit

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,16 @@
+import { LayoutModule } from './layout/layout.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LayoutModule
+  ]
+})
+export class AdminModule { }
